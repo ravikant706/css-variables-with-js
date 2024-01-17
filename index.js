b0e5ba8a39fd55a0e9a680ed3da8c823e19a03fb
ravikant706 <ravikant706@gmail.com>
2024-01-17 23:39:52 +0530

    final code done

---
+++ index.js
@@ -0,0 +1,11 @@
+var inputs = document.querySelectorAll('.controls input');
+
+function handelUpdate() {
+    const suffix = this.dataset.sizing || '';
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+
+
+}
+
+inputs.forEach(input => input.addEventListener('change', handelUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handelUpdate));
